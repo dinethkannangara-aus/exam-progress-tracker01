676becbeaad33395d7d40bd00b2ce4060d755ab1
dinethkannangara-aus <skfwmngw4r@privaterelay.appleid.com>
2026-05-26 19:26:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/past-paper-tracker/",
-});
+  base: '/exam-progress-tracker01/',
+  plugins: [react()],
+})
